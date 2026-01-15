7314904c612ce76abdbaa0121cc4f0fa411e603a
mahmudamg11sm <mahmudamg11@gmail.com>
2026-01-16 00:59:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 
 # ====== CONFIG ======
-TOKEN = "YOUR_BOT_TOKEN"  # Saka token dinka nan
+TOKEN = "TOKEN"  # Saka token dinka nan
 ADMIN_ID = 6648308251      # Saka Telegram ID ɗinka nan
 
 bot = telebot.TeleBot(TOKEN)
